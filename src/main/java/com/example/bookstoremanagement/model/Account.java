3e33e5ea0d31e3f7eafe440fc5fe8f32efb317b8
ductinh0402 <hoangductinh0402@gmail.com>
2022-06-20 09:10:36 +0700

    tinh push

--- src/main/java/com/example/bookstoremanagement/model/Account.java
+++ src/main/java/com/example/bookstoremanagement/model/Account.java
@@ -1,22 +1,43 @@
 package com.example.bookstoremanagement.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "Account",uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank
+    @Size(min=3,max=50)
+    private String name;
+    @Size(min=3,max=50)
+    private String username;
     private String email;
+    @JsonIgnore
     private String password;
+    @Lob
+    private String avatar;
     private Boolean isEnabled;
 
     @ManyToMany(fetch = FetchType.EAGER)
@@ -33,4 +54,10 @@ public class Account {
     private Customer customer;
 
 
+    public Account(String name, String username, String email, String encode) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = encode;
+    }
 }
